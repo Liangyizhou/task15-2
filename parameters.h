ddf7992abf24aa45e41d760d31e63ef419ed20f4
Liangyizhou <61281453+Liangyizhou@users.noreply.github.com>
2020-06-13 15:40:37 +0800

    Add files via upload

---
+++ parameters.h
@@ -0,0 +1,12 @@
+#pragma once
+#pragma once
+
+
+//选择需要演示的demo
+#define DEMO_METHOD		1			//0:yolov3 demo	1:openpose demo	
+
+//参数设置
+#define YOLOV3_VIDEO		"1.mp4"		
+#define OPENPOSE_VIDEO		"1.mp4"		
+
+//#define INPUT_METHOD			#pragma once
